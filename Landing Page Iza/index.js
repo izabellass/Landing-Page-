c229372f646b06cbc813e0687069ad668c3b3162
izabellass <163480289+izabellass@users.noreply.github.com>
2024-03-15 13:54:46 -0400

    Add files via upload

---
+++ Landing Page Iza/index.js
@@ -0,0 +1,14 @@
+let nome = "Joao"
+
+const alturaEmM = "1.8"
+const anoAtual = "2024"
+const idade = "30"
+
+const anoNascimento = anoAtual - idade 
+
+
+console.log(anoNascimento)
+console.log(nome)
+
+console.log("meu nome é", nome, "e eu nasci em", anoNascimento,);
+
